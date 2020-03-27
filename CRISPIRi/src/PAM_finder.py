56b73489260649035d74c2028bb87bea6b4d19f4
Yunfei Dai <yunfei@Yunfeis-MacBook-Pro.local>
2020-03-27 04:42:27 -0400

    fixed bug

--- CRISPIRi/src/PAM_finder.py
+++ CRISPIRi/src/PAM_finder.py
@@ -38,17 +38,26 @@ def find_pam(sequence, length=20, outfile="pam_list"):
         for record in SeqIO.parse(f, "fasta"):                  # read each sequence (chromosome)
             seq_ID = record.id
             POS_PAM = {}
+            counter1 = 0
+            counter2 = 0
             for PAM_NGG in re.finditer("GG", str(record.seq)):  # search PAM in plus strand ending with "...NGG"
                 if PAM_NGG.start() >= 23:
                     GG_pos = PAM_NGG.start() + 1                # position of GG
-                    pam_start_plus = GG_pos - length - 2        # start position of the PAM sequence
+                    pam_start_plus = GG_pos - length - 1        # start position of the PAM sequence
                     pam_end_plus = GG_pos - 2                   # end position of the PAM sequence
                     POS_PAM.update({pam_start_plus: [seq_ID, pam_end_plus, plus_strand]})
+                    counter1 += 1
+            print("Found " + str(counter1) + " PAM sequences on \"+\" strand")
             for PAM_CCN in re.finditer("CC", str(record.seq)):  # search PAM in minus strand following "CCN..."
                 CC_pos = PAM_CCN.start() + 1
                 pam_start_minus = CC_pos + 3
-                pam_end_minus = CC_pos + length + 3
+                pam_end_minus = CC_pos + length + 2
                 POS_PAM.update({pam_start_minus: [seq_ID, pam_end_minus, minus_strand]})
+                counter2 += 1
+            print("Found " + str(counter2) + " PAM sequences on \"-\" strand")
+            print("Found " + str(counter1+counter2) + " PAM sequences in total from both strands")
+            counter1 = 0
+            counter2 = 0
             PAM_list = []
             for key in POS_PAM:
                 PAM_list.append([POS_PAM[key][0], key, POS_PAM[key][1], POS_PAM[key][2]])
