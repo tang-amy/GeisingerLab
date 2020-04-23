8688a1c3ca4094d7068621cbc69584e5ced5221b
Yunfei Dai <yunfei@Yunfeis-MacBook-Pro.local>
2020-04-22 20:55:10 -0400

    extracts additional information

--- CRISPRi/src/clean_Prados_TSS.py
+++ CRISPRi/src/clean_Prados_TSS.py
@@ -16,17 +16,22 @@ locus_tag = df_prados['following_ORF_Name'].tolist()
 cluster_id = df_prados['TSS_cluster_ID'].tolist()
 strand = df_prados['strand'].tolist()
 pos = df_prados['position'].tolist()
+inside_ORF = df_prados['is_inside_an_ORF'].tolist()
+distance_ORF = df_prados['following_ORF_distance'].tolist()
 strand_dic = {'Plus':'+', 'Minus':'-'}
-
+ORF_dic = {0:'No', 1:'Yes'}
 tss = []
 for i in index:
     coordinate = pos[i]
     tag = locus_tag[i]
     s = strand_dic[strand[i]]
     cluster = cluster_id[i]
-    tss.append([coordinate, tag, s, cluster])
+    distance = distance_ORF[i]
+    inside = ORF_dic[inside_ORF[i]]
+    tss.append([coordinate, tag, s, cluster, inside, distance])
 
 
-df = pd.DataFrame(tss, columns=['TSS coordinate', 'Locus_tag', 'Strand', 'TSS_cluster_ID'])
+df = pd.DataFrame(tss, columns=['TSS coordinate', 'Locus tag', 'Strand', 'TSS cluster ID',
+                                'is_inside_ORF', 'Distance to ORF'])
 df.to_csv(outfile, sep='\t', index=False)
 
