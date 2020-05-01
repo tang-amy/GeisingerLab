eca2cfc29676f499b255154f07239fced1bce639
Yunfei Dai <dai.yun@husky.neu.edu>
2020-05-01 18:37:22 -0400

    reformatted

--- Chip-Seq_Analysis/src/batch_call_peak.sh
+++ Chip-Seq_Analysis/src/batch_call_peak.sh
@@ -18,7 +18,9 @@ do for i in 1 2 3
 		do fname=$(basename $file);
 		oname=${OUT}/${fname%%.sorted.bam};
 		input=${DIR}/seed$seed/input/*28-$i*
-		macs2 callpeak -t $file -c $input -g 3.8e6 -n $oname.input_28-$i.ext200 --nomodel --extsize 200 >> ${OUT}/macs2_log.txt 2>&1;
+		macs2 callpeak -t $file -c $input -g 3.8e6 \
+		-n $oname.input_28-$i.ext200 \
+		--nomodel --extsize 200 >> ${OUT}/macs2_log.txt 2>&1;
 		done	
 	done
 done
