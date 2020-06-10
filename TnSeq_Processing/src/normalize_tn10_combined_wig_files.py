b5e81ebb84ceee109eb75aa344144da4dc42ee1c
Yunfei Dai <dai.yun@husky.neu.edu>
2020-06-10 10:33:26 -0400

    added support for command-line arguments

--- TnSeq_Processing/src/normalize_tn10_combined_wig_files.py
+++ TnSeq_Processing/src/normalize_tn10_combined_wig_files.py
@@ -8,17 +8,29 @@
 3) for each, output as a 2-column text file with just column 1 and the value resulting after the division. add the header
 "variableStep chrom=NZ_CP012004" so it can be used in transit and igv as wig
 
+Numpy and pandas (version 0.24.0 or above) are required.
+
+Usage:
+
+python normalize_tn10_combined_wig_files.py [INPUT] [OUTPUT DIRECTORY]
+
 """
 import pandas as pd
 import numpy as np
+from sys import argv
+import os
 
-input = '/Volumes/Seagate/TN10-TTR-combined_wig_files/TN10-TTR-combined_wig_output-20200609.txt'
-df = pd.read_csv(input, sep='\t', skiprows=[i for i in range(0, 27)],
+INPUT = argv[1] if len(argv)>1 else "/Volumes/Seagate/TN10-TTR-combined_wig_files/TN10-TTR-combined_wig_output-20200609.txt"
+OUTPUT = argv[2] if len(argv)>2 else "/Volumes/Seagate/TN10-TTR-combined_wig_files"
+base = '.'.join(os.path.abspath(INPUT).split('/')[-1].split('.')[:-1])
+
+df = pd.read_csv(INPUT, sep='\t', skiprows=[i for i in range(0, 27)],
                  engine='python', header=None)
 v = df.iloc[:, 1:24].to_numpy(dtype='float')
 total = np.sum(v, axis=1)
 for i in [500, 1000, 1500, 2000, 2500]:
-    output = '/Volumes/Seagate/TN10-TTR-combined_wig_files/denomenator_' + str(i) + '_TN10-TTR-combined_wig_output-20200609.txt'
+    outname = 'denominator_' + str(i) + '_' + base + '.wig'
+    output = os.path.join(OUTPUT, outname)
     with open(output, 'w+') as f:
         f.write('variableStep chrom=NZ_CP012004\n')
     division = np.true_divide(total, i)
