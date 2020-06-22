2809ab0db0273f1a371c5099366cb96a63c6b07f
Yunfei Dai <dai.yun@husky.neu.edu>
2020-06-22 01:02:13 -0400

    batch run bowtie

--- Chip-Seq_Analysis/src/batch_bowtie.sh
+++ Chip-Seq_Analysis/src/batch_bowtie.sh
@@ -5,10 +5,18 @@ REF=${2?Error: "enter directory of reference files (.ebwt), e.g. Ab17978/Ab17978
 SAM_OUTPUT="$(dirname $DIR)/mapped_SAM_files"
 BAM_OUTPUT="$(dirname $DIR)/sorted_BAM_files"
 
-mkdir -p ${OUTPUT}
+mkdir -p ${SAM_OUTPUT} ${BAM_OUTPUT}
 
 parallel bowtie -m 1 -n 1 --best -y -S $REF {} {.}.sam ::: ${DIR}/*.fastq
+printf "Finished sequence alignment.\n"
 
 for file in ${DIR}/*.sam
-do mv $file ${OUT}/${file/clipped}
+do fname=$(basename ${file})
+mv $file ${SAM_OUTPUT}/${fname/clipped_}
 done
+
+for file in ${SAM_OUTPUT}/*.sam
+do fname=$(basename ${file})
+samtools sort $file -o ${BAM_OUTPUT}/${fname/.sam/_sorted.bam}
+done
+printf "Finished sorting SAM files.\n"
