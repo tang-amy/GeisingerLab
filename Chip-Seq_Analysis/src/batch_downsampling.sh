51de37211e42603f17666cbf5d5e32feece17186
Yunfei Dai <dai.yun@husky.neu.edu>
2020-06-22 01:49:01 -0400

    batch downsampling

--- Chip-Seq_Analysis/src/batch_downsampling.sh
+++ Chip-Seq_Analysis/src/batch_downsampling.sh
@@ -9,28 +9,29 @@
   exact read number from each downsampled files are counted and writted into downsampling_log.txt
 
   Usasge (order of arguments matters!):
-  bash batch_downsampling.sh [INPUT Directory] [OUTPUT Directory] [Target Read Number]
+  bash batch_downsampling.sh [INPUT Directory] 
 Comment
 
 DIR=${1?Error: "requires directory of bam files"}
-OUT=${2?Error: "provide output directory"}
-READS=${3?Error: "provide target read number for downsampling"}
-
+read -p "Please enter the target read number:" READS
+OUT=$(dirname ${DIR})/downsampled_BAM_files
 
 for seed in 2 5 9
 do
-  mkdir ${OUT}/seed$seed ${OUT}/seed$seed/input;
+  mkdir -p ${OUT}/seed${seed} ${OUT}/seed${seed}/input;
   for file in $DIR/*.bam
   do
-    fname=$(basename $file);
+    read_count=$(samtools view ${file} | wc -l);
+    scale=$(echo "scale=6; (${READS}/${read_count})+${seed}" | bc);
+    rt=$(echo "scale=1; (${READS}/1000000)" | bc)
+    fname=$(basename ${file});
     oname=${fname/sorted.bam/s${seed}_${rt}M.sorted.bam};
-    read_count=$(samtools view $file | wc -l);
-    scale=$(echo "scale=6; ($READS/$read_count)+$seed" | bc);
-    rt=$(echo "scale=1; ($READS/1000000)" | bc)
     ofile=${OUT}/seed${seed}/${oname};
     samtools view -s $scale -b $file -o $ofile;
     echo ${fname} >> ${OUT}/downsampling_${rt}M_log.txt;
     echo "s${seed}_${rt}M: $(samtools view $ofile | wc -l) reads" >> ${OUT}/downsampling_${rt}M_log.txt;
     done
-  mv ${OUT}/seed$seed/*Input*.bam ${OUT}/seed$seed/input/
+  mv ${OUT}/seed${seed}/*Input*.bam ${OUT}/seed${seed}/input/
   done
+
+printf "Finished downsampling. \n"
