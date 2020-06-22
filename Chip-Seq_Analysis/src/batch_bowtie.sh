06e0d61b235111718da970ce52567517a633f1bf
yunfei-dai <44348205+yunfei-dai@users.noreply.github.com>
2020-06-22 01:06:36 -0400

    Update batch_bowtie.sh

--- Chip-Seq_Analysis/src/batch_bowtie.sh
+++ Chip-Seq_Analysis/src/batch_bowtie.sh
@@ -10,11 +10,14 @@ mkdir -p ${SAM_OUTPUT} ${BAM_OUTPUT}
 parallel bowtie -m 1 -n 1 --best -y -S $REF {} {.}.sam ::: ${DIR}/*.fastq
 printf "Finished sequence alignment.\n"
 
+# sequence alignment for all sam files in $DIR
 for file in ${DIR}/*.sam
 do fname=$(basename ${file})
+# move files to SAM_OUTPUT
 mv $file ${SAM_OUTPUT}/${fname/clipped_}
 done
 
+# sort bam files
 for file in ${SAM_OUTPUT}/*.sam
 do fname=$(basename ${file})
 samtools sort $file -o ${BAM_OUTPUT}/${fname/.sam/_sorted.bam}
