cee26169fc2b516aacf1632e5225473e742e3ca3
Amy Tang <33613127+tang-amy@users.noreply.github.com>
2020-07-11 01:47:16 -0400

    changed parsing of each line (was 1-off before)

--- TnSeq_Processing/src/sam_to_wig.py
+++ TnSeq_Processing/src/sam_to_wig.py
@@ -30,9 +30,9 @@ def make_read_counter(samfile):
     f.close()
     for i in flines:
         linelist = i.split()
-        strand = int(linelist[2]) & 0b00010000
-        coordinate = int(linelist[4])
-        length = len(linelist[10])
+        strand = int(linelist[1]) & 0b00010000
+        coordinate = int(linelist[3])
+        length = len(linelist[9])
         if strand == 0: # + strand << flag for reverse complement is off
             coordinate += length
         else: # - strand << flag for reverse complement is on
