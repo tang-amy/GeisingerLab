141bd8831daf30a9f7076e763406e63c754ba0da
yunfei-dai <44348205+yunfei-dai@users.noreply.github.com>
2020-11-05 17:15:56 -0500

    Update batch_call_peak.sh

--- Chip-Seq_Analysis/src/batch_call_peak.sh
+++ Chip-Seq_Analysis/src/batch_call_peak.sh
@@ -7,10 +7,18 @@ DIR=${1?Error: "enter directory of fastq files"}
 OUT=${2?Error: "provide output directory name"}
 mkdir -p ${OUT}/narrowPeak ${OUT}/xls ${OUT}/bed
 
-for file in ${DIR}/*ChIP-??-$i*
-do fname=$(basename $file);
-oname=${OUT}/${fname%%.sorted.bam};
-input=${DIR}/seed$seed/input/*28-$i*;
+for i in 1 2 3
+        do for file in JBA71FLAG_BEADS_${i}_*.bam
+                fname=$(basename $file);
+                oname=${OUT}/${fname%%.sorted.bam};
+                input=${DIR}/JBA71_INPUT_${i}_*.bam;
+                echo $fname;
+                echo $oname;
+                echo $input;
+                done
+         done
+             
+'''
 macs2 callpeak -t $file -c $input -g 3.8e6 \
 -n $oname.input_28-$i.ext200 \
 --nomodel --extsize 200 >> ${OUT}/macs2_log.txt 2>&1;
@@ -19,3 +27,4 @@ done
 mv ${OUT}/*.narrowPeak ${OUT}/narrowPeak
 mv ${OUT}/*.xls ${OUT}/xls
 mv ${OUT}/*.bed ${OUT}/bed
+'''
