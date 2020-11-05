5cf57fb57e23bdb80be6b2ffc1fe2155bdcbc66a
Yunfei Dai <dai.yun@husky.neu.edu>
2020-11-05 02:17:39 -0500

    Allow call peak for raw sample (not downsampled)

--- Chip-Seq_Analysis/src/batch_call_peak.sh
+++ Chip-Seq_Analysis/src/batch_call_peak.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
 ## Yunfei Dai
-## 04/28/2020
+## 11/05/2020
 
-<<Comment
-  Run macs2 with downsample treatment files against input.
-  Usage:
-  bash batch_call_peak.sh <input directory> <output directory>
-Comment
-
-DIR=${1?Error: "requires directory of bam files"}
+DIR=${1?Error: "enter directory of fastq files"}
 OUT=${2?Error: "provide output directory name"}
-mkdir ${OUT}/narrowPeak ${OUT}/xls ${OUT}/bed
-for seed in 2 5 9
-do for i in 1 2 3
-	do for file in ${DIR}/seed$seed/*ChIP-??-$i*
-		do fname=$(basename $file);
-		oname=${OUT}/${fname%%.sorted.bam};
-		input=${DIR}/seed$seed/input/*28-$i*
-		macs2 callpeak -t $file -c $input -g 3.8e6 \
-		-n $oname.input_28-$i.ext200 \
-		--nomodel --extsize 200 >> ${OUT}/macs2_log.txt 2>&1;
-		done	
-	done
-done
+mkdir -p ${OUT}/narrowPeak ${OUT}/xls ${OUT}/bed
+
+for file in ${DIR}/*ChIP-??-$i*
+do fname=$(basename $file);
+oname=${OUT}/${fname%%.sorted.bam};
+input=${DIR}/seed$seed/input/*28-$i*;
+macs2 callpeak -t $file -c $input -g 3.8e6 \
+-n $oname.input_28-$i.ext200 \
+--nomodel --extsize 200 >> ${OUT}/macs2_log.txt 2>&1;
+done    
+
 mv ${OUT}/*.narrowPeak ${OUT}/narrowPeak
 mv ${OUT}/*.xls ${OUT}/xls
 mv ${OUT}/*.bed ${OUT}/bed
---
+++ Chip-Seq_Analysis/src/batch_call_peak_downsampled.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Yunfei Dai
+## 04/28/2020
+
+<<Comment
+  Run macs2 with downsample treatment files against input.
+  Usage:
+  bash batch_call_peak.sh <input directory> <output directory>
+Comment
+SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=${1?Error: "requires directory of bam files"}
+OUT=${2?Error: "provide output directory name"}
+for seed in 2 5 9
+        do for i in 1 2 3
+                do bash $SRC/batch_call_peak.sh ${DIR}/seed$seed ${OUT}
+                done	
+        done
