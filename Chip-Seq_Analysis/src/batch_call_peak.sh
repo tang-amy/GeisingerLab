8785462948ed8f3b6c5861b987faddbb637c6c3d
yunfei-dai <44348205+yunfei-dai@users.noreply.github.com>
2020-11-05 17:17:08 -0500

    Update batch_call_peak.sh

--- Chip-Seq_Analysis/src/batch_call_peak.sh
+++ Chip-Seq_Analysis/src/batch_call_peak.sh
@@ -4,16 +4,16 @@
 ## 11/05/2020
 
 DIR=${1?Error: "enter directory of fastq files"}
-OUT=${2?Error: "provide output directory name"}
+#OUT=${2?Error: "provide output directory name"}
 mkdir -p ${OUT}/narrowPeak ${OUT}/xls ${OUT}/bed
 
 for i in 1 2 3
         do for file in JBA71FLAG_BEADS_${i}_*.bam
                 fname=$(basename $file);
-                oname=${OUT}/${fname%%.sorted.bam};
+                #oname=${OUT}/${fname%%.sorted.bam};
                 input=${DIR}/JBA71_INPUT_${i}_*.bam;
                 echo $fname;
-                echo $oname;
+                #echo $oname;
                 echo $input;
                 done
          done
