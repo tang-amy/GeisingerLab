a59cb5c4070a876d7f030aec71457865765ac9ec
yunfei-dai <44348205+yunfei-dai@users.noreply.github.com>
2021-02-04 14:57:32 -0500

    Update unzip-clip_fastq.sh
    fixed fastx_clipper

--- TnSeq_Processing/src/unzip-clip_fastq.sh
+++ TnSeq_Processing/src/unzip-clip_fastq.sh
@@ -4,29 +4,21 @@
 # will make all directories/files in parent directory of zipped files
 
 DIR=${1:-$PWD}
-
-# creating var for output directories
+read -p "Please enter adaptor sequence:" ADAPTOR
 UNZIPPED="$(dirname $DIR)/unzipped_fastq_files"
 CLIPPED="$(dirname $DIR)/clipped_fastq_files"
+mkdir -p $UNZIPPED $CLIPPED
 
-#unzipping files
-#keeps original gz zipped file
-mkdir -p $UNZIPPED
-for file in $DIR/*.gz
-do gunzip -k $file
-done
+# unzip all .gz files in $DIR
+parallel gunzip -k {} ::: $DIR/*.gz
+printf "Finished unzipping files, now clipping adaptors.\n"
+printf "Adapator sequence $ADAPTOR will be removed from reads.\n"
 for file in $DIR/*.fastq
-do mv "$file" $UNZIPPED
+do mv $file $UNZIPPED
 done
 
-echo "Finished unzipping all .gz files into .fastq files."
-
-#clips files and adds clipped files to its own directory
-mkdir -p $CLIPPED
-for file in $UNZIPPED/*.fastq
-do
-fastx_clipper -v -l 20 -a [adapter sequence] -i $file -o $CLIPPED/clipped_$(basename $file)
-# parallel 'fastx_clipper -v -l 20 -a [adapter sequence] -i {} -o ${CLIPPED}/clipped_{}' ::: $UNZIPPED/*.fastq
-done
-
-echo "Finished clipping all .fastq files with fastx_clipper."
+# clips adaptors from *.fastq files in #UNZIPPED
+cd $UNZIPPED
+parallel fastx_clipper -v -l 20 -d 0 -Q 33 -a $ADAPTOR -i {} -o ${CLIPPED}/clipped_{} ::: *.fastq
+printf "Finished clipping adaptors.\n"
+cd $DIR
