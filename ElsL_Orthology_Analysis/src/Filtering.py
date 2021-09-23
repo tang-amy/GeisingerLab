7f0bb8f408d6e7b3259dbf5b9836095f3f9ace29
yunfei-dai <44348205+yunfei-dai@users.noreply.github.com>
2021-09-23 01:20:07 -0400

    Update Filtering.py

--- ElsL_Orthology_Analysis/src/Filtering.py
+++ ElsL_Orthology_Analysis/src/Filtering.py
@@ -118,7 +118,8 @@ def get_CDD_domains(query):
             domains.append(df_CDD_domains.domain_name[entry])
     return domains
 """
-print(df_CDD_domains.head())
+
+df_CDD_domains['Query'] = df_CDD_domains['Query'].astype(str)
 df_CDD_domains = df_CDD_domains.groupby(df_CDD_domains.Query)['domain_name'].apply(",".join).reset_index()
 df_CDD_domains.set_index('Query', inplace=True)
 
